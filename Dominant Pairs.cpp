ca6e2afb52f0785e26135f72c313d0b650fb3a12
Gurmeet <gurmeetsingh222003@gmail.com>
2023-04-12 01:58:00 +0530

    Dominant Pairs - 12 April 2023 - GFG POTD

---
+++ Dominant Pairs.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dominantPairs(int n,vector<int> &arr){
+        sort(arr.begin(),arr.begin()+n/2,greater<int>{});
+        sort(arr.begin()+n/2,arr.end(),greater<int>{});
+        int i=0,j=n/2;
+        int count=0;
+        while(i<n/2 && j<n)
+        {
+            if(arr[i]>=5*arr[j])
+            {
+                count+=(n-j);
+                i++;
+            }
+            else 
+            {
+                j++;
+            }
+        }
+        return count;
+    } 
