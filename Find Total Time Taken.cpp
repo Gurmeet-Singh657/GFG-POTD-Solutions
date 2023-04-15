2efe479e922f61b53dc5012be87a6d7a32340b46
Gurmeet <gurmeetsingh222003@gmail.com>
2023-04-15 17:18:20 +0530

    Find the total time taken - 15 April 2023 - GFG POTD

---
+++ Find Total Time Taken.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int totalTime(int n, vector<int> &arr, vector<int> &time) {
+       int ans=0;
+       unordered_set<int> st; // store repeated elements
+       st.insert(arr[0]);
+       for(int i=1;i<n;i++)
+       {
+           if(st.find(arr[i])!=st.end()) // repeated
+           {
+               ans+=time[arr[i]-1];
+           }
+           else // pick the element
+           {
+               ans++;
+           }
+           st.insert(arr[i]);
+       }
+       return ans;
+    }
