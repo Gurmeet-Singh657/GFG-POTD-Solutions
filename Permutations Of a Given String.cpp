811be071003edfea7ffe73fad3725b160f03a677
Gurmeet <gurmeetsingh222003@gmail.com>
2023-06-09 14:55:00 +0530

    Permutations of a given String - GFG POTD Solution - 9 June 2023

---
+++ Permutations Of a Given String.cpp
@@ -0,0 +1,10 @@
+vector<string>find_permutation(string S)
+{
+    vector<string> ans;
+    sort(S.begin(),S.end());
+    do
+    {
+        ans.push_back(S);
+    }while(next_permutation(S.begin(),S.end()));
+    return ans;
+}
