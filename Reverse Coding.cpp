b0b1db73cf1fe719ece94bf82434d14f5d9edcc1
Gurmeet <gurmeetsingh222003@gmail.com>
2023-06-21 16:51:05 +0530

    Reverse Coding - Updated - GFG POTD Solution - 21 June 2023

--- Reverse Coding.cpp
+++ Reverse Coding.cpp
@@ -1,16 +1,5 @@
 #define mod (int)(1e9 + 7)
 int sumOfNaturals(int n)
 {
-    int first, second;
-    if (n % 2 == 0)
-    {
-        first = n / 2;
-        second = n + 1;
-    }
-    else
-    {
-        first = (n + 1) / 2;
-        second = n;
-    }
-    return (first * 1LL * second) % mod;
+    return ((n*1LL*(n+1))/2)%mod;
 }
