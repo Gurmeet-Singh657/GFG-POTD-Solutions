923cbb17951995ae3187e1a0043420201ecd6e82
Gurmeet <gurmeetsingh222003@gmail.com>
2023-07-10 02:55:53 +0530

    Transpose of Matrix - GFG POTD Solution - 10 July 2023

---
+++ Transpose of Matrix.cpp
@@ -0,0 +1,10 @@
+void transpose(vector<vector<int>> &matrix, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+}
