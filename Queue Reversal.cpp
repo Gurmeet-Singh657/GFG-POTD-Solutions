39207af721c491cafff293efe31f9b0e3b8b50aa
Gurmeet <gurmeetsingh222003@gmail.com>
2023-07-16 03:33:35 +0530

    Queue Reversal - GFG POTD Solution - 16 July 2023

---
+++ Queue Reversal.cpp
@@ -0,0 +1,15 @@
+queue<int> rev(queue<int> q)
+{
+    stack<int> st;
+    while(!q.empty())
+    {
+        st.push(q.front());
+        q.pop();
+    }
+    while(!st.empty())
+    {
+        q.push(st.top());
+        st.pop();
+    }
+    return q;
+}
